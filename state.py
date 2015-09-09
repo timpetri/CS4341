b0727735958fb0ed78f247fc9f4a80645894c7bb
Tim <tapetri@wpi.edu>
2015-09-09 15:13:47 -0400

    Changes to State and getSuccessors function

--- state.py
+++ state.py
@@ -3,21 +3,18 @@
 
 """
 
-class state:
+class State:
 	
 	north = "north"
 	south = "south"
 	east = "east"
 	west = "west"
 
-	def __init__(self, world, heuristic, startX, startY, goalX, goalY, direction = this.north, score = 0, actionList = list()):
+	def __init__(self, world, posX, posY, goalX, goalY, direction, score , actionList):
 		# global
 		this.world = world
-		this.heuristic = heuristic
 
 		# world
-		this.startX = startX
-		this.startY = startY	
 		this.goalX = goalX
 		this.goalY = goalY
 
@@ -34,4 +31,53 @@ class state:
 		"""	Params: none
 			Return: a list of states
 		"""
+
+		successors = []
+
+		# add forward
+		successors.append(self.performForward())
+		# add bash
+
+		# add turn left
+
+		# add turn right
+
+		# add demolish
+
 		pass
+
+	def performForward(self):
+		newState = State(self)
+
+		# add forward to action list
+		newState.actionList.append("forward")
+
+		# set new position of agent
+		if self.direction is this.north:
+			newState.posY += 1
+		else if self.direction is this.east:
+			newState.posX += 1
+		else if self.diretion is this.south:
+			newState.posY -= 1
+		else if self.direction is this.west
+			newState.posX -= 1
+
+		# decrement score by time complexity of new square
+		newState.score -= world[newState.posX][newState.posY]
+	
+	def performBash(self):
+
+
+	def performTurnLeft(self):
+
+	def performTurnRight(self):
+
+	def performDemolish(self):
+	
+
+
+
+
+
+
+
