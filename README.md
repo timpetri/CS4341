e4a8a38344175e55a75c83711a391f822abd2478
nsbradford <nsbradford@gmail.com>
2015-09-09 16:28:34 -0400

    Finished loading maps in astar.py

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CS4341
 Course assignments in CS4341 at WPI
+
+
+Coordinate System:
+	The world is noted in (y, x) starting from the top left, as is customary with matrices.
--- astar.py
+++ astar.py
@@ -1,13 +1,51 @@
+#!/usr/bin/python
+
 import sys
 import os
+from state import State
 
 DEBUG = True
+charStartState = "S"
+charGoalState = "G"
+
+def printWorld(world):
+	print "World loaded: "
+	for line in world:
+		for element in line:
+			print element,
+	print ""
 
 def loadInputFile(fileName):
-	"""Loads the input file into a 2D array"""
+	"""Returns a 2D array"""
 	if DEBUG: print "Loading..."
+	inputFile = open(fileName, "r")
+	world = []
+	count = 0
 
-	
+	while True:
+		line = inputFile.readline()
+		if not line: break
+		values = line.split("\t")
+		world.append(list())
+		for value in values:
+			world[count].append(value)
+		count += 1
+	printWorld(world)
+	return world
+
+def findCharInWorld(world, charState):
+	"""Returns a tuple (y,x) of the coordinates of the designated char."""
+	for y in range (len(world)):
+		for x in range (len(world[0])):
+			if world[y][x] == charState:
+				print "Found " + charState + "\t y=" + str(y) + ", x=" + str(x) 
+				break #return (1,2) #x, y
+	return y, x
+
+def solve(initialState, heuristic):
+	#answer = aStarSearch(initialState, heuristic)
+	#print str(answer)	
+	pass
 
 if __name__ == "__main__":
 	str_usage = "Usage: python astar.py <input_file>.txt <heuristic 1-6>"
@@ -20,4 +58,8 @@ if __name__ == "__main__":
 	elif not os.path.isfile(fileName):
 		raise IOError("Input file does not exist.")
 	else:
-		loadInputFile(fileName)
+		world = loadInputFile(fileName)
+		startX, startY = findCharInWorld(world, charStartState)
+		goalX, goalY = findCharInWorld(world, charGoalState)
+		initialState = State() # ( State.north, 0, list() )
+		solve(initialState, None)
