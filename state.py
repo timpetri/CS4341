fef2dce67a5b7eb4053b88619e2cf93ba421e10d
nsbradford <nsbradford@gmail.com>
2015-09-09 14:37:29 -0400

    Modified state.py for Tim.

--- state.py
+++ state.py
@@ -1,15 +1,37 @@
+"""
+	state.py 
+
+"""
+
 class state:
-	def __init__(self, world, heuristic, startX, startY, goalX, goalY, score = 0, actionList = list()):
+	
+	north = "north"
+	south = "south"
+	east = "east"
+	west = "west"
+
+	def __init__(self, world, heuristic, startX, startY, goalX, goalY, direction = this.north, score = 0, actionList = list()):
+		# global
 		this.world = world
 		this.heuristic = heuristic
-		this.posX = startX
-		this.posY = startY
-		this.score = score
 
-class world:
-	def __init__(self, field, startX, startY, goalX, goalY)
-		this.field = field
+		# world
 		this.startX = startX
-		this.startY = startY
+		this.startY = startY	
 		this.goalX = goalX
 		this.goalY = goalY
+
+		# agent
+		this.posX = startX
+		this.posY = startY
+		this.direction = direction
+
+		# important
+		this.score = score
+		this.actionList = actionList
+
+	def getSuccessors(self):
+		"""	Params: none
+			Return: a list of states
+		"""
+		pass
