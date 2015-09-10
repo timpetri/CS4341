ee73bc3c591864a63b6fc6f7c1ef1266e1e638bd
Thomas Grimshaw <magwitch324@gmail.com>
2015-09-10 16:13:51 -0400

    Changed the admissible heuristic to turn amounts

--- heuristic.py
+++ heuristic.py
@@ -18,39 +18,71 @@ def aStar(initialState, heuristic):
             return nextState.actionList
         else:
             for successorState in nextState.getSuccessors():
-                print "adding state with score " + str(successorState.score)
+                #print "adding state with score " + str(successorState.score)
                 fringe.put((-(successorState.score - heuristic(successorState)), successorState))
 
-            for elem in list(fringe.queue):
+            """for elem in list(fringe.queue):
                 print(elem[0]),
             print ""
+            """
 
 def zeroHeuristic(state):
     """0: Returns 0."""
     return 0
 
 def minHeuristic(state):
-    xdiff, ydiff = goalDiff(state)
+    xdiff, ydiff = absGoalDiff(state)
     return min(xdiff, ydiff)
 
 def maxHeuristic(state):
-    xdiff, ydiff = goalDiff(state)
+    xdiff, ydiff = absGoalDiff(state)
     return max(xdiff, ydiff)
 
 def addHeuristic(state):
-    xdiff, ydiff = goalDiff(state)
+    xdiff, ydiff = absGoalDiff(state)
     return xdiff + ydiff
 
 def admissibleHeuristic(state):
-    x, y, z = state.getNextValues();
-    return addHeuristic(state) - 1 + min(x, y, z)
+    xdiff, ydiff = goalDiff(state)
+
+    turnCost = 0
+
+    if state.direction is state.north:
+        if xdiff is not 0:
+            turnCost +=1
+        if ydiff > 0:
+            turnCost += 2
+
+    if state.direction is state.east:
+        if xdiff < 0:
+            turnCost += 2
+        if ydiff is not 0:
+            turnCost +=1
+
+    if state.direction is state.south:
+        if xdiff is not 0:
+            turnCost +=1
+        if ydiff < 0:
+            turnCost += 2
+
+    if state.direction is state.west:
+        if xdiff > 0:
+            turnCost += 2
+        if ydiff is not 0:
+            turnCost +=1
+
+    return addHeuristic(state) + turnCost
     
 def nonAdmissibleHeuristic(state):
     """6: multiply #5 * 3."""
-    return admissibelHeuristic(state)*3
+    return admissibleHeuristic(state)*3
+
+def absGoalDiff(state):
+    xdiff, ydiff = goalDiff(state)
+    return (abs(xdiff), abs(ydiff))
 
 def goalDiff(state):
     """Helper for returning distance to goal."""
-    xdiff = abs(state.goalX - state.posX)
-    ydiff = abs(state.goalY - state.posY)
+    xdiff = state.goalX - state.posX
+    ydiff = state.goalY - state.posY
     return (xdiff, ydiff)
