756062b2f7a87e9b16de1598fe1527f495228ddd
nsbradford <nsbradford@gmail.com>
2015-09-22 16:08:48 -0400

    towerPuzzle has population generation and mutation working.

--- A2/towerPuzzle.py
+++ A2/towerPuzzle.py
@@ -1,89 +1,131 @@
 """
 	towerPuzzle.py
+	Author: Nicholas S. Bradford
 
 """
 
-from random import randint, random
+import re
+from random import randint, sample
 from abstractPuzzle import AbstractPuzzle
 
-"""
-Puzzle 3: TowerPuzzle
 
-"""
 class TowerPuzzle(AbstractPuzzle):
+	"""Puzzle 3: TowerPuzzle"""
 
 	def __init__(self):
-		# global 2d array
-		# self.test = 1
-		pass
+		""" Constructor. The initialTowerList is initialized in generateInitialPopulation()."""
+		self.initialTower = None
+
+	def parseInitialTower(self, inputText):
+		"""	Input: Takes inputText in as a list of text file lines.
+			Output: A list of pieces that can be used to generate new Towers.
+
+		"""
+		print "Loading input file..."
+		_answerList = list()
+
+		for line in inputText:
+			if not line: break
+			_values = re.split(r'[;,\s]\s*', line)
+			#print str(_values)
+			_piece = Piece(_values[0], _values[1], _values[2], _values[3])
+			print _piece
+			_answerList.append(_piece)
+		#print str(_answerList)
+		print "Finished loading input.\n"
+		return _answerList
+
+	def generateRandomTower(self):
+		"""	Generates a tower with a random size (>= 2) and random pieces)."""
+		_pieceList = sample(self.initialTower.pieceList, randint(2, len(self.initialTower.pieceList)))
+		return Tower(_pieceList)
 
 	def generateInitialPopulation(self, popSize, inputText):
 		""" Input: Takes inputText in as a list of text files lines.
-			Returns: """
-
+			Returns: 
 		"""
-		for x in xrange(popSize):
-			x1 = randint(0, 150)
-			x2 = randint(0, 150)
-			x3 = randint(0, 150)
-			x4 = randint(0, 150)
-			x5 = randint(0, 150)
 
-			population.append([x1, x2, x3, x4, x5])
-		"""
-		population = []
-		for line in inputText:
-			population.append(line)
-		return population
+		self.initialTower = Tower(self.parseInitialTower(inputText))
+		assert self.initialTower.containsDoorAndLookout(), "Error: Tower does not contain a door and lookout."
+		_population = []
+		
+		for i in xrange(popSize):
+			_population.append(self.generateRandomTower())
+		for tower in _population:
+			print tower
+		return _population
 
 	def createChild(self, parent1, parent2):
-		child = parent1[2:] + parent2[:2]
-
-		#print "Created child " + str(child) + " from parents " + str(parent1) + " and " + str(parent2)
+		""" Returns: a child taken by taking the first half of parent1 and the second half of parent2."""
+		child = Tower(parent1.pieceList[:len(parent1.pieceList)/2] + parent2.pieceList[len(parent2.pieceList)/2-1:])
+		print "Created child: " + str(child) + " from parents: " + str(parent1) + " and " + str(parent2)
 		return child
 
 	def mutate(self, individual):
 		"""	Input: member of the population.
-			Returns: individual with a random change. Validity of resulting mutation not enforced."""
-		pos = randint(0, len(individual)-1)
-
-		individual[pos] = randint(0, 100)
-
+			Returns: individual with a random change. Validity of resulting mutation not enforced.
+				The random change will be a swap of the positions of two pieces in the Tower.
+		"""
+		assert isinstance(individual, Tower), "Error: Passed a non-Tower into a TowerPuzzle.score() individual." 
+		pos1 = randint(0, len(self.initialTower.pieceList) - 1)
+		pos2 = randint(0, len(self.initialTower.pieceList) - 1)
+		individual.pieceList[pos1], individual.pieceList[pos2] = individual.pieceList[pos2], individual.pieceList[pos1]
 		return individual
 
 	def fitness(self, individual):
 		"""	Input: member of the population.
 			Returns: the fitness of the individual (not necessarily the score)."""
+		assert isinstance(individual, Tower), "Error: Passed a non-Tower into a TowerPuzzle.score() individual." 
 		return self.score(individual)
 
 	def score(self, individual):
 		""" Input: member of the population.
 			Returns: the score of the individual. Returns 0 for invalid individuals."""
-		total = 0
-		for x in individual:
-			total += x
+		assert isinstance(individual, Tower), "Error: Passed a non-Tower into a TowerPuzzle.score() individual." 
+		if individual.containsDoorAndLookout():
+			return 1
+		else:
+			return 0
+		#total = 0
+		#for x in individual:
+		#	total += x
+		# 200 - abs(200 - total)
 
-		return 200 - abs(200 - total)
+# End class TowerPuzzle
 
 class Tower():
 	"""Represents a list of Piece objects, which form a Tower when stacked."""
 
 	def __init__(self, pieceList):
 		for piece in pieceList:
-			assert (type(piece) is Piece), "Error: Passed a non-piece into a Tower() pieceList." 
-		self.pieceList = pieceList
+			assert isinstance(piece, Piece), "Error: Passed a non-piece into a Tower() pieceList." 
+		self.pieceList = list(pieceList) #make sure that you use all new references
 
-	def printTower(self):
-		for piece in pieceList:
-			piece.printPiece()
+	def __str__(self):
+		return str([str(x) for x in self.pieceList])
 
+	def containsDoorAndLookout(self):
+		""" Returns: True if the Tower has a Door and Lookout somewhere in it."""
+
+		hasDoor = False
+		hasLookout = False
+		for piece in self.pieceList:
+			if not hasDoor and piece.pieceType == Piece._door:
+				hasDoor = True
+			elif not hasLookout and piece.pieceType == Piece._lookout:
+				hasLookout = True
+			if hasLookout and hasDoor:
+				break
+		return hasDoor and hasLookout
+
+# End class Tower
 
 class Piece():
 	"""Represents a piece used to build the tower."""
 
-	_door = "door"
-	_wall = "wall"
-	_lookout = "lookout"
+	_door = "Door"
+	_wall = "Wall"
+	_lookout = "Lookout"
 	_pieceTypes = [_door, _wall, _lookout]
 
 	def __init__(self, pieceType, width, length, cost):
@@ -95,5 +137,10 @@ class Piece():
 		self.length = length
 		self.cost = cost
 
-	def printPiece(self):
-		print self.pieceType + "\tw:" + str(self.width) + "\tl:" + str(self.length) + "\tc:" + str(self.cost)
+	def __str__(self):
+		#return self.pieceType + "\twid:" + str(self.width) + "\tlen:" + str(self.length) + "\tcost:" + str(self.cost)
+		return self.pieceType + " " + str(self.width) + " " + str(self.length) + " " + str(self.cost)
+
+# End class Piece
+
+#EOF
